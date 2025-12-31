c761c1b80d0d3e6f1bc78c908c7fd7b63278a762
Atiksh Chawla <124847414+AtikshC@users.noreply.github.com>
2025-12-31 17:23:12 -0500

    Create next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  basePath: "/campus-lost-found",
+  assetPrefix: "/campus-lost-found/",
+  images: {
+    unoptimized: true,
+  },
+};
+
+export default nextConfig;
